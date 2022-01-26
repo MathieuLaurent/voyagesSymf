c1a32e95456020edd80a86b277ce3a57b3739143
Laurent Mathieu <mathieu.laurent1995@gmail.com>
2022-01-26 15:33:24 +0100

    menu

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('admin/category')]
-class CategoryController extends AbstractController
+class AdminCategoryController extends AbstractController
 {
     #[Route('/', name: 'admin_category_index', methods: ['GET'])]
     public function index(CategoryRepository $categoryRepository): Response
