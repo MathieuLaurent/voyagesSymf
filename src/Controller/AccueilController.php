2adbc8fabef1da2728bdc3934899fc823e00b053
Laurent Mathieu <mathieu.laurent1995@gmail.com>
2022-01-26 16:54:30 +0100

    fin de journée :p

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Voyage;
+use App\Form\VoyageType;
+use Doctrine\DBAL\Types\TextType;
 use App\Repository\VoyageRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
     public function index(VoyageRepository $voyages): Response
     {
+        $form = $this->createForm(VoyageType::class);
+        $form->handleRequest();
+
+
         return $this->render('pages/accueil.html.twig', [
             'voyages' => $voyages->findAll(),
         ]);
